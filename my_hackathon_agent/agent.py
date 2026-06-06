9d2448816a515fe8d4428e467f094dbcbdbd6076
Aventurine-web <noellyon9@gmail.com>
2026-06-06 20:55:18 +0200

    Fixed emailing feature and env file managment

--- my_hackathon_agent/agent.py
+++ my_hackathon_agent/agent.py
@@ -1,4 +1,27 @@
+import os
 from google.adk.agents.llm_agent import Agent
+import smtplib
+from email.message import EmailMessage
+
+def send_email(to: str, title: str, message: str) -> str:
+    """You can use this function to send an email."""
+
+    Email_address = os.getenv("EMAIL_ADDRESS")
+    Email_password = os.getenv("EMAIL_PASSWORD")
+    
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = title
+    msg['From'] = Email_address
+    msg['To'] = to
+
+    try:
+        with smtplib.SMTP("://gmail.com", 465) as smtp:
+            smtp.login(Email_address, Email_password)
+            smtp.send_message(msg)
+            return f"Email sent to {to} with title '{title}'"
+    except Exception as e:
+        return f"Failed to send email: {str(e)}"
 
 def hamta_status(team_namn: str) -> str:
     """"Du är en hackathon-assistent. Om användaren frågar om ett team, använd ALLTID verktyget hämta_status för att få rätt information."""
