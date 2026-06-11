125ba61f73a067c8f7535903b8ae872b210879d3
Aventurine-web <noellyon9@gmail.com>
2026-06-11 15:19:28 +0200

    Fixed full english version, and a simple db using sqlite3

--- my_hackathon_agent/agent.py
+++ my_hackathon_agent/agent.py
@@ -3,6 +3,8 @@ import os
 from google.adk.agents.llm_agent import Agent
 from google.adk.tools import google_search
 import smtplib
+import sqlite3
+import json
 from email.message import EmailMessage
 
 def send_email(to: str, title: str, message: str) -> str:
@@ -24,17 +26,34 @@ def send_email(to: str, title: str, message: str) -> str:
             return f"Email sent to {to} with title '{title}'"
     except Exception as e:
         return f"Failed to send email: {str(e)}"
+    # database start -------
+def init_db():
+    conn = sqlite3.connect('hackathon.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS teams 
+                 (name TEXT, project TEXT, status TEXT)''')
+    conn.commit()
+    conn.close()
+init_db()
 
-def hamta_status(team_namn: str) -> str:
-    """Du är en hackathon-assistent. Om användaren frågar om ett team, använd ALLTID verktyget hämta_status för att få rätt information."""
-    # Här kan du ha kod som kollar en databas eller en hemsida
-    return f"Team {team_namn} är just nu i full gång med att bygga sin agent!"
+def Save_info(team_name: str, project: str, status: str) -> str:
+    """You are a hackathon assistant. When a user asks about a team, always use the tool Save_info to get correct information."""
+    # code for database connection and saving the information
+    try:
+        conn = sqlite3.connect('hackathon.db')
+        c = conn.cursor()
+        c.execute("INSERT OR REPLACE INTO teams (name, project, status) VALUES (?, ?, ?)", (team_name, project, status))
+        conn.commit()
+        conn.close()
+        return f"Information of {team_name} saved in database!"
+    except Exception as e:
+        return f"Failed to save information: {str(e)}" 
 
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your functions ability.',
-    tools=[hamta_status, send_email],
+    tools=[Save_info, send_email],
 )
 
